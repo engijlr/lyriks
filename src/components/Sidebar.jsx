0ff679591a04c5da92dd1bc4cda578a12317acfa
Engi Lozada <engilozada@gmail.com>
2023-03-18 17:25:03 +0100

    Create PlayPause component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
 
 export default Sidebar;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", process.env.RAPID_API_KEY);
+      headers.set("X-RapidAPI-Key", "...");
       return headers;
     },
   }),
